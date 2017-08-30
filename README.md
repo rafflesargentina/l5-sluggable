d2b84fe04b52c57789eb93f7b492da9c2a915498
Mario Patronelli <mario@raffles.com.ar>
2017-08-30 12:56:56 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# l5-sluggable
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Total Downloads][ico-downloads]][link-downloads]
+
+Unique slug generator for Laravel 5. Clone of Caffeinated Slugs.
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require rafflesargentina/l5-sluggable
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) and [CONDUCT](CONDUCT.md) for details.
+
+## Security
+
+If you discover any security related issues, please email mario@raffles.com.ar instead of using the issue tracker.
+
+## Credits
+
+- [Mario Patronelli][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/rafflesargentina/l5-sluggable.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/rafflesargentina/l5-sluggable.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/rafflesargentina/l5-sluggable
+[link-downloads]: https://packagist.org/packages/rafflesargentina/l5-sluggable
+[link-author]: https://github.com/patronelli87
+[link-contributors]: ../../contributors
---
+++ src/SluggableTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace RafflesArgentina\Sluggable;
+
+trait SluggableTrait
+{
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($model) {
+            $slugName  = static::getSlugName();
+            $slugField = static::getSlugField();
+            $model->$slugField = str_slug($model->$slugName);
+            $latestSlug = static::whereRaw("{$slugField} RLIKE '^{$model->slug}(-[0-9]*)?$'")
+                ->latest('id')
+                ->value($slugField);
+            if ($latestSlug) {
+                $pieces = explode('-', $latestSlug);
+                $number = intval(end($pieces));
+                $model->$slugField .= '-'.($number + 1);
+            }
+        });
+    }
+
+    /**
+     * Get the name field associated for slugs.
+     *
+     * @return string
+     */
+    public static function getSlugName()
+    {
+        if (null !== static::$slug['name']) {
+            return static::$slug['name'];
+        }
+        return 'name';
+    }
+
+    /**
+     * Get the slug field associated for slugs.
+     *
+     * @return string
+     */
+    public static function getSlugField()
+    {
+        if (null !== static::$slug['field']) {
+            return static::$slug['field'];
+        }
+        return 'slug';
+    }
+}
